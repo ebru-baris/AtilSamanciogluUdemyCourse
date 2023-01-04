6bb45c5f6a18ba1f79153006db39f984a01048fc
Ebru Baris <114153844+ebru-baris@users.noreply.github.com>
2023-01-04 08:48:33 +0300

    Add files via upload

---
+++ TimerProject/TimerProject/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  TimerProject
+//
+//  Created by Ebru Barış on 25.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var myLabel: UILabel!
+    var timer = Timer()
+    var counter = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        counter = 10
+        myLabel.text = " Time: \(counter)"
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
+        
+    }
+    @objc func timerFunction() {
+        myLabel.text = "Time: \(counter)"
+        counter -= 1
+        if counter == 0 {
+            timer.invalidate()
+            myLabel.text = "Time's Over"
+        }
+    }
+
+
+    @IBAction func buttonClicked(_ sender: Any) {
+    }
+}
+
